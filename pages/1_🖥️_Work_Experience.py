316c8e169efa0f49b9650034e4bd86cbddd88379
Raye92 <ca.raychen@gmail.com>
2024-08-22 18:34:31 -0700

    Random Forest Model added, captions adjusted, added raw data display

--- pages/1_🖥️_Work_Experience.py
+++ pages/1_🖥️_Work_Experience.py
@@ -1,20 +1,21 @@
 import streamlit as st
 from utils import show_code
 
-subpages = ["Decision Tree", "Model 2"]
+subpages = ["Regression Models", "Model 2"]
 
 def work_page():
     st.set_page_config(page_title = "Work Experience", page_icon = "🖥️")
 
     st.write("### Melbourne Housing Price Prediction")
-    st.markdown("""This Decision Tree Regression model is trained on Melbourne housing data to predict housing prices.
-    The model uses feature optimization, tuned with leaf node sizes to minimize Mean Absolute Error.
+    st.markdown("""Two models I worked on at Data Annotation are Decision Tree and Random Forest models.
+    These regression models are trained on Melbourne housing data to predict housing prices.
+    The models use feature optimization, fitted with leaf node optimization to minimize mean absolute error (MAE).
     """)
     st.write("  \n")
     st.write("##### Actual vs. Predicted Prices")
 
     subpage = st.sidebar.selectbox("Projects at Data Annotation Tech", subpages)
-    if subpage == "Decision Tree":
+    if subpage == "Regression Models":
         decision_tree()
         show_code(decision_tree)
     elif subpage == "Model 2":
@@ -27,6 +28,7 @@ def decision_tree():
     import time
     import matplotlib.pyplot as plt
     from sklearn.tree import DecisionTreeRegressor, plot_tree
+    from sklearn.ensemble import RandomForestRegressor
     from sklearn.metrics import mean_absolute_error
     from sklearn.model_selection import train_test_split
 
@@ -54,21 +56,26 @@ def decision_tree():
     maes = {max_leaf_node: get_mae(max_leaf_node, train_X, val_X, train_y, val_y) for max_leaf_node in max_leaf_nodes}
     best_num_nodes = min(maes, key=maes.get)
 
-    melb_model = DecisionTreeRegressor(max_leaf_nodes=best_num_nodes) # Could use random_state param
-    melb_model.fit(train_X, train_y)
-    predictions = melb_model.predict(val_X).round(0)
+    decision_tree = DecisionTreeRegressor(max_leaf_nodes=best_num_nodes) # Could use random_state param
+    random_forest = RandomForestRegressor(max_leaf_nodes=800) # Tested and fitted with optimal num
+
+    decision_tree.fit(train_X, train_y)
+    random_forest.fit(train_X, train_y)
+    tree_preds = decision_tree.predict(val_X).round(0)
+    forest_preds = random_forest.predict(val_X).round(0)
 
     # Display model results
     results_df = pd.DataFrame( {
         'Actual': val_y,
-        'Prediction': predictions
+        'DT Prediction': tree_preds,
+        'RF Prediction': forest_preds
     })
 
     progress_bar = st.sidebar.progress(0)
     status_text = st.sidebar.empty()
 
     total_rows = results_df.shape[0]
-    chart = st.line_chart(results_df.iloc[[0,1]], color=["#834cba", "#eb6e21"])
+    chart = st.line_chart(results_df.iloc[[0,1]], color=["#834cba", "#eb6e21", "#44c2b3"])
 
     for row in range(1, total_rows):
         chart.add_rows(results_df.iloc[[row-1, row]])
@@ -79,15 +86,24 @@ def decision_tree():
 
     progress_bar.empty()
 
+    st.button("Regenerate")
+    st.write("")
+
     # Display mae
-    mae = mean_absolute_error(val_y, predictions)
-    st.write("Mean Absolute Error: $", mae)
+    dt_mae = mean_absolute_error(val_y, tree_preds)
+    rf_mae = mean_absolute_error(val_y, forest_preds)
+    st.write("Decision Tree Mean Absolute Error: $", dt_mae)
+    st.write("Random Forrest MAE: $", rf_mae)
+    st.write("We can see the use of multiple tree components generally results in a more accurate result.")
+    st.write("")
 
     # Display data
-    st.write("Model Vs. Actual Housing Prices")
+    st.write("Model Vs. Actual Housing Prices: ")
     st.dataframe(results_df.T)
-    st.write("Feature Values")
+    st.write("Feature Values: ")
     st.dataframe(val_X.T, height = 200)
+    st.write("Original Melbourne Housing Data (Before preprocessing, cleaning, and feature engineering): ")
+    st.dataframe(data, height = 150)
 
     # Sample tree diagram
     st.write("  \n #### Simplified Tree Model:")
